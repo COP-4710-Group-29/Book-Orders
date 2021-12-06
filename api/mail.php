dfeb3d3a03f0ee432e6349eb9cf896481c9118ea
Sarah Myerson <myersonsarah1999@gmail.com>
2021-12-06 11:23:42 -0500

    Syntax and directory changes
    Fixed syntax errors.
    Fixed missing composer vendor location.
    Fixed link sent to user's email.

--- api/mail.php
+++ api/mail.php
@@ -10,13 +10,12 @@ require '../PHPMailer/src/PHPMailer.php';
 require '../PHPMailer/src/SMTP.php';
 
 //Load Composer's autoloader
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 require 'connect.php';
 
-if(isset($_POST["email"])) 
+if(isset($_POST["email"]))
 {
-
     $email = $_POST["email"];
 
     // Check if the email already exists
@@ -31,9 +30,9 @@ if(isset($_POST["email"]))
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
     // If it returns anything != 0, throw an error
-    if($row['num'] != 0)
+    if($row['num'] != 1)
     {
-        $array = array("status"=>false,"message"=>"An account with that email does not exists!.");
+        $array = array("status"=>false,"message"=>"An account with that email does not exist!");
         echo json_encode($array);
         die();
     }
@@ -46,49 +45,48 @@ if(isset($_POST["email"]))
     $stmt->bindValue(':code', $code);
     $stmt->bindValue(':email', $email);
 
-    if($result)
+    $result = $stmt->execute();
+
+    if(!$result)
     {
-        $array = array("status"=>true);
-        echo json_encode($array);
+      $array = array("status"=>false,"message"=>"An error occured.");
+      echo json_encode($array);
+      exit();
     }
-    else
-    {
-    $array = array("status"=>false,"message"=>"An error occured.");
-        echo json_encode($array);
-        exit();
-    }
-   
+
     $result = $stmt->execute();
 
     $mail = new PHPMailer(true);
 
     try {
         //Server settings
-        $mail->isSMTP();                                            
-        $mail->Host       = 'smtp.gmail.com';                     
-        $mail->SMTPAuth   = true;                                   
-        $mail->Username   = 'cop4710group29@gmail.com';                     
-        $mail->Password   = 'kinghts23!';                               
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
-        $mail->Port       = 465;                                    
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'cop4710group29@gmail.com';
+        $mail->Password   = 'knights23!';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465;
 
         //Recipients
         $mail->setFrom('cop4710group29@gmail.com', 'UCF Library');
-        $mail->addAddress($email);     
+        $mail->addAddress($email);
         $mail->addReplyTo('no-reply@gmail.com', 'No Reply');
 
         //Content
-        $url = "http://" + $_SERVER["HTTP_HOST"] . dirname($_SERVER["PHP_SELF"]) . "/accounts/reset?code=$code";
-        $mail->isHTML(true);                                  
+        $url = "http://" . $_SERVER["HTTP_HOST"] . "/accounts/reset?code=$code";
+        $mail->isHTML(true);
         $mail->Subject = 'Password Recovery link';
         $mail->Body    = '<h1>You reuested a password recovery</h1>
-                            Click <a href='$url'> HERE </a> to change your password';
+                            Click <a href='.$url.'> HERE </a> to change your password';
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
         $mail->send();
-        echo 'Reset link was sent to your email';
+        $array = array("status"=>true,"message"=>"Reset link was sent to your email.");
+        echo json_encode($array);
     } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        $array = array("status"=>false,"message"=>"Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+        echo json_encode($array);
     }
 }
 ?>
